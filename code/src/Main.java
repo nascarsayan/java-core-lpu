2b00bd1a40ab1101ddd92a1b825ec207c4feb5a6
Sayan Naskar <nascarsayan@gmail.com>
2025-01-14 16:38:12 +0530

    renamed variable from x to z

--- code/src/Main.java
+++ code/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         // create a new variable
-        int x = 10;
+        int z = 10;
         // print the value of the variable
-        System.out.println(x);
+        System.out.println(z);
         System.out.println("Hello, Mars!");
     }
 }
