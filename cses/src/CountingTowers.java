7c48c475ea6224e28094aad4bcae28b1490a24b7
Sayan Naskar <nascarsayan@gmail.com>
2025-01-18 10:06:13 +0530

    counting towers

--- cses/src/CountingTowers.java
+++ cses/src/CountingTowers.java
@@ -1,35 +1,3 @@
----
-theme: default
-class: 'text-center'
-transition: slide-left
-title: Day 4
-exportFilename: Day_04.pdf
-mdc: true
----
-
-# Day 4
-
-# There is no magic, every convenience is just an abstraction
-
-<div class="pt-12">
-  <span @click="$slidev.nav.next" class="px-2 py-1 rounded cursor-pointer" flex="~ justify-center items-center gap-2" hover="bg-white bg-opacity-10">
-    Press Space for next page <div class="i-carbon:arrow-right inline-block"/>
-  </span>
-</div>
-
----
-layout: default
----
-
-# Table of contents
-
-<Toc columns=3></Toc>
-
----
-
-# Counting Towers
-
-```java
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -94,4 +62,3 @@ public class CountingTowers {
         }
     }
 }
-```
